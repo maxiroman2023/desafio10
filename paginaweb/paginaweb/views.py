14117351192219e992f359f08c27462a0af668f1
Maxi Roman <133285169+maxiroman2023@users.noreply.github.com>
2023-06-29 19:46:56 -0300

    Delete paginaweb directory

--- paginaweb/paginaweb/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-def index(request):
-        return render(request, 'index.html')
-
-def usuarios(request):
-        nombres = ['Juan', 'María', 'Pedro']
-        return render(request, 'usuarios.html', {'nombres': nombres})
